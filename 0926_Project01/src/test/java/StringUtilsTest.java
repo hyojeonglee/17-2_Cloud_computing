8192f817bbf1808bb73e063573d53ed18198b8b1
hyojeonglee <hyon2224@naver.com>
2017-09-26 19:17:02 +0900

    add common lang library

---
+++ 0926_Project01/src/test/java/StringUtilsTest.java
@@ -0,0 +1,15 @@
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+public class StringUtilsTest {
+	
+	// ctrl + space = help
+	@Test
+	public void testIsBlank() {
+		String a = null;
+		
+		if (StringUtils.isBlank(a)) {
+			
+		}
+	}
+}
