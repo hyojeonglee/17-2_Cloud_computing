3deb9f512b5cc27a33784f496c6fb2cccfccd548
hyojeonglee <hyon2224@naver.com>
2017-09-26 19:35:49 +0900

    Refactor StringUtilsTest

--- 0926_Project01/src/test/java/StringUtilsTest.java
+++ 0926_Project01/src/test/java/StringUtilsTest.java
@@ -1,15 +1,36 @@
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class StringUtilsTest {
-	
-	// ctrl + space = help
 	@Test
 	public void testIsBlank() {
-		String a = null;
 		
+		Assert.assertTrue(StringUtils.isBlank(null));
+		
+		///////////////
+		// Shortcuts //
+		///////////////
+		/* ctrl + space = help
+		 * F3 = Function manual
+		 * sysout + control + space */
+		
+		
+		////////////////////////
+		// Before Refactoring //
+		////////////////////////
+		/* if (StringUtils.isBlank(a)) {
+			System.out.println("a is blank.");
+		} */
+	}
+	
+	@Test
+	public void testIsBlank2() throws Exception {
+		String a = null;
 		if (StringUtils.isBlank(a)) {
-			
+			throw new Exception(); // Error 발생
 		}
 	}
+	// main 함수가 따로 없어도 @Test annotation이 있으면
+	// JUnit 프레임워크에서 실행된다!
 }
